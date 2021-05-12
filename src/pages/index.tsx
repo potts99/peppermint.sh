474f0f19ba03899ff31991c81998910b5486dc13
potts99 <japotts16@gmail.com>
2021-05-13 00:42:04 +0100

    comment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import ListSection from '@/components/list-section';
 import FeatureSection from '@/components/feature-section';
 import CasesSection from '@/components/cases-section';
 import SocialProof from '@/components/social-proof';
-import PricingTable from '@/components/pricing-table';
+// import PricingTable from '@/components/pricing-table';
 import Footer from '@/components/footer';
 
 export default function Home() {
